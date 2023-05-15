24ee8df688034525d446ae6c793027171caa448e
Choijake <shameless8@naver.com>
2023-05-15 22:05:25 +0900

    Initial commit

---
+++ src/main/java/com/example/studyproject/StudyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studyproject/StudyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
