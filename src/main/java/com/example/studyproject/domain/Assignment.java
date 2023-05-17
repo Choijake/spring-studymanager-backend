5765654fbd637efdc46f8739f92cb4b416c55596
Yeon Kyung Ryu <celina16161616@gmail.com>
2023-05-17 17:56:16 +0900

    과제(assignment) 엔티티 클래스 생성

---
+++ src/main/java/com/example/studyproject/domain/Assignment.java
@@ -0,0 +1,58 @@
+package com.example.studyproject.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Assignment {
+
+
+    //PK 지정
+    @Id
+    //데이터베이스에 따라 자동으로 ID가 지정 - 기본 세팅
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column
+    private String startdate;
+    public String getStartdate() {
+        return startdate;
+    }
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    @Column
+    private String finishdate;
+    public String getFinishdate() {
+        return finishdate;
+    }
+    public void setFinishdate(String finishdate) {
+        this.finishdate = finishdate;
+    }
+
+    @Column
+    private float progress;
+    public float getProgress() {
+        return progress;
+    }
+    public void setProgress(float progress) {
+        this.progress = progress;
+    }
+
+
+}
